952cb7b73a6a975054ec189d0e97e82057ecfd23
wmorgan <wmorgan@5c8cc53c-5e98-4d25-b20a-d8db53a31250>
2007-01-08 20:26:35 +0000

    dump exception to exception-log.txt (useful because it has the COMPLETE backtrace)
    git-svn-id: svn://rubyforge.org/var/svn/sup/trunk@230 5c8cc53c-5e98-4d25-b20a-d8db53a31250

--- lib/sup.rb
+++ lib/sup.rb
@@ -34,6 +34,10 @@ module Redwood
       begin
         yield
       rescue Exception => e
+        File.open("exception-log.txt", "w") do |f|
+          f.puts "--- #{e.class.name} at #{Time.now}"
+          f.puts e.message, e.backtrace
+        end
         $exception ||= e
         raise
       end
