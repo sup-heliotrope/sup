affd74834a1a2620646e9ca12ecf18f19414cda4
wmorgan <wmorgan@5c8cc53c-5e98-4d25-b20a-d8db53a31250>
2007-09-14 01:50:33 +0000

    log message cleanup
    git-svn-id: svn://rubyforge.org/var/svn/sup/trunk@568 5c8cc53c-5e98-4d25-b20a-d8db53a31250

--- lib/sup/thread.rb
+++ lib/sup/thread.rb
@@ -313,7 +313,6 @@ class ThreadSet
   ## may do nothing if m's thread is killed
   def load_thread_for_message m, opts={}
     good = @index.each_message_in_thread_for m, opts do |mid, builder|
-      Redwood::log "  > got #{mid}"
       next if contains_id? mid
       add_message builder.call
     end
