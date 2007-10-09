468ce1e79a6068c4ed1c0a20b6ae4ac7b361be29
wmorgan <wmorgan@5c8cc53c-5e98-4d25-b20a-d8db53a31250>
2007-10-09 14:03:18 +0000

    fix message id space issue
    git-svn-id: svn://rubyforge.org/var/svn/sup/trunk@610 5c8cc53c-5e98-4d25-b20a-d8db53a31250

--- lib/sup/message.rb
+++ lib/sup/message.rb
@@ -58,8 +58,10 @@ class Message
 
     @from = PersonManager.person_for header["from"]
 
-    @id = header["message-id"].gsub(/^\s+|\s+$/, "")
-    unless @id
+    @id = header["message-id"]
+    if @id
+      id.gsub!(/^\s+|\s+$/, "")
+    else
       @id = "sup-faked-" + Digest::MD5.hexdigest(raw_header)
       Redwood::log "faking message-id for message from #@from: #@id"
     end
