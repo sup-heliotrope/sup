2d48a91089f3000ee20cdf0ccf2f362ab780d269
wmorgan <wmorgan@5c8cc53c-5e98-4d25-b20a-d8db53a31250>
2007-11-27 17:31:54 +0000

    bump version to 0.3
    git-svn-id: svn://rubyforge.org/var/svn/sup/trunk@726 5c8cc53c-5e98-4d25-b20a-d8db53a31250

--- lib/sup.rb
+++ lib/sup.rb
@@ -32,7 +32,7 @@ class Module
 end
 
 module Redwood
-  VERSION = "0.2"
+  VERSION = "0.3"
 
   BASE_DIR   = ENV["SUP_BASE"] || File.join(ENV["HOME"], ".sup")
   CONFIG_FN  = File.join(BASE_DIR, "config.yaml")
