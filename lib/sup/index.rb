0f8ca5cd4104afc9f2d805f8a6d33b8d6d2e85c6
William Morgan <wmorgan-sup@masanjin.net>
2009-05-26 14:22:37 -0700

    fencepost bug: thread size wrong in log

--- lib/sup/index.rb
+++ lib/sup/index.rb
@@ -389,7 +389,7 @@ EOS
       Redwood::log "thread for #{m.id} is killed, ignoring"
       false
     else
-      Redwood::log "ran #{num_queries} queries to build thread of #{messages.size + 1} messages for #{m.id}: #{m.subj}" if num_queries > 0
+      Redwood::log "ran #{num_queries} queries to build thread of #{messages.size} messages for #{m.id}: #{m.subj}" if num_queries > 0
       messages.each { |mid, builder| yield mid, builder }
       true
     end
