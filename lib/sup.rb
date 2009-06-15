3abb06b4c0eac63bb4e0372fb835a9c00311fcae
William Morgan <wmorgan-sup@masanjin.net>
2009-06-15 07:56:13 -0700

    bump to 0.8

--- lib/sup.rb
+++ lib/sup.rb
@@ -33,7 +33,7 @@ class Module
 end
 
 module Redwood
-  VERSION = "git"
+  VERSION = "0.8"
 
   BASE_DIR   = ENV["SUP_BASE"] || File.join(ENV["HOME"], ".sup")
   CONFIG_FN  = File.join(BASE_DIR, "config.yaml")
