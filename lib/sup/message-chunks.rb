09a8cdae3778c8605956046fa129571bf83f4453
Benoît PIERRE <benoit.pierre@gmail.com>
2009-08-12 00:09:40 +0200

    don't redirect stderr to /dev/null when calling run-mailcap
    Some programs will fail to work correctly if this is done, like for
    example: w3m --dump, to view HTML files.

--- lib/sup/message-chunks.rb
+++ lib/sup/message-chunks.rb
@@ -131,7 +131,7 @@ EOS
     def initial_state; :open end
     def viewable?; @lines.nil? end
     def view_default! path
-      cmd = "/usr/bin/run-mailcap --action=view '#{@content_type}:#{path}' 2>/dev/null"
+      cmd = "/usr/bin/run-mailcap --action=view '#{@content_type}:#{path}'"
       Redwood::log "running: #{cmd.inspect}"
       system cmd
       $? == 0
