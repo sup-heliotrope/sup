e280c88a9df624ab1d3651d3d789aaca481ade1f
Christopher Bertels <bakkdoor@flasht.de>
2009-10-02 16:34:43 +0200

    default to english translation if it is missing for the current language.

--- lib/sup/i18n.rb
+++ lib/sup/i18n.rb
@@ -10,25 +10,42 @@ module Redwood
     def initialize lang=:en, base_path="i18n/"
       @lang = lang
       @base_path = base_path
-      read_language_file
+      @translations = {}
+      read_language_file @lang
+      read_default_language
     end
 
-    def read_language_file
-      lang_fn = @base_path + "/#{@lang}.yaml"
-      @translations = YAML::load_file lang_fn
+    def read_language_file lang
+      lang_fn = @base_path + "/#{lang}.yaml"
+      @translations[lang.to_sym] = YAML::load_file lang_fn
+    end
+
+    ## default language is english (:en)
+    def read_default_language
+      read_language_file :en unless @translations[:en]
     end
 
     ## access translation values via I18n['foo.bar']
     def [] translation_key, replacements = {}
-      val = @translations
-      translation_key.split(".").each do |x|
-        if val.is_a? Hash
-          val = val[x]
-        end
-      end
+      val = translation_for translation_key, @lang
+      val = translation_for(translation_key, :en) unless val # load default translation (english) if none found
+      val ||= "No translation found for: #{translation_key}"
+
       ## substitute values, if needed
       replacements.each do |k,v|
         val = val.gsub "#\{#{k}\}", v.to_s
+      end
+      val
+    end
+
+    def translation_for translation_key, lang
+      val = @translations[lang]
+      translation_key.split(".").each do |x|
+        if val && (val.is_a? Hash)
+          val = val[x]
+        else
+          return nil # value not found within translations
+        end
       end
       val
     end
