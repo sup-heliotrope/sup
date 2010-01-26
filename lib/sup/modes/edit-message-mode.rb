c2f8184f05d38bc3c447c004c93c1df34955a7a5
Daniel Schoepe <daniel.schoepe@googlemail.com>
2010-01-26 16:45:16 +0100

    Added crypto-settings hook

--- lib/sup/modes/edit-message-mode.rb
+++ lib/sup/modes/edit-message-mode.rb
@@ -40,6 +40,18 @@ Return value:
 	none
 EOS
 
+  HookManager.register "crypto-mode", <<EOS
+Modifies cryptography settings based on header and message content, before
+editing a new message. This can be used to set, for example, default cryptography
+settings.
+Variables:
+    header: a hash of headers. See 'signature' hook for documentation.
+    body: an array of lines of body text.
+    crypto_selector: the UI element that controls the current cryptography setting.
+Return value:
+     none
+EOS
+
   attr_reader :status
   attr_accessor :body, :header
   bool_reader :edited
@@ -92,6 +104,9 @@ EOS
     add_selector @crypto_selector if @crypto_selector
     
     HookManager.run "before-edit", :header => @header, :body => @body
+    if @crypto_selector
+      HookManager.run "crypto-mode", :header => @header, :body => @body, :crypto_selector => @crypto_selector
+    end
 
     super opts
     regen_text
