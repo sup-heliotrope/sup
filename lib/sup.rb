210fd40b59a3cb6f5df6cfa6caf87db4129248d8
Gaute Hope <eg@gaute.vetsj.com>
2013-04-15 09:33:52 +0200

    Use supmua.org for YAML_DOMAIN

--- lib/sup.rb
+++ lib/sup.rb
@@ -52,7 +52,7 @@ module Redwood
   SEARCH_FN  = File.join(BASE_DIR, "searches.txt")
   LOG_FN     = File.join(BASE_DIR, "log")
 
-  YAML_DOMAIN = "masanjin.net"
+  YAML_DOMAIN = "supmua.org"
   YAML_DATE = "2006-10-01"
 
   ## record exceptions thrown in threads nicely
