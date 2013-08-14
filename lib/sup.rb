f4f3ec7de9795b2b4311dcbda016f2b2968c5631
Gaute Hope <eg@gaute.vetsj.com>
2013-08-14 19:59:54 +0200

    Ensure correct encoding of default user name when no config is present

--- lib/sup.rb
+++ lib/sup.rb
@@ -271,7 +271,7 @@ EOM
     else
       require 'etc'
       require 'socket'
-      name = Etc.getpwnam(ENV["USER"]).gecos.split(/,/).first rescue nil
+      name = Etc.getpwnam(ENV["USER"]).gecos.split(/,/).first.force_encoding($encoding).fix_encoding rescue nil
       name ||= ENV["USER"]
       email = ENV["USER"] + "@" +
         begin
@@ -283,8 +283,8 @@ EOM
       config = {
         :accounts => {
           :default => {
-            :name => name,
-            :email => email,
+            :name => name.fix_encoding,
+            :email => email.fix_encoding,
             :alternates => [],
             :sendmail => "/usr/sbin/sendmail -oem -ti",
             :signature => File.join(ENV["HOME"], ".signature"),
