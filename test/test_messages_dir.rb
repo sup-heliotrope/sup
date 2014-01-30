1a68681ebae0b5684140d8cd675800dbaf02e726
Gaute Hope <eg@gaute.vetsj.com>
2014-01-30 11:06:05 +0100

    test: crappy address field in spam message

--- test/test_messages_dir.rb
+++ test/test_messages_dir.rb
@@ -27,6 +27,18 @@ end
 
 module Redwood
 
+  # monkey patch the MBox source to work without the index.
+  require 'sup/mbox'
+  class MBox
+    def first_new_message
+      0
+    end
+
+    def my_ensure_open
+      @f = File.open @path, 'rb' if @f.nil?
+    end
+  end
+
 class TestMessagesDir < ::Minitest::Unit::TestCase
 
   def setup
@@ -142,7 +154,6 @@ class TestMessagesDir < ::Minitest::Unit::TestCase
 
   end
   def test_weird_header_encoding
-    require 'sup/util'
     message = ''
     File.open 'test/messages/weird-encoding-in-header-field.eml', "r:UTF-8" do |f|
       message = f.read
@@ -183,6 +194,49 @@ class TestMessagesDir < ::Minitest::Unit::TestCase
 
     # check if body content includes some of the expected text
     assert (lines.join.include? "check out: "), "Body message does not match expected value"
+  end
+
+
+
+  def test_bad_address_field
+
+    testmsg = File.join(File.dirname(__FILE__), 'messages/bad-address-spam.eml')
+    source = MBox.new ("mbox:///#{testmsg}")
+    source.my_ensure_open
+    source_info = 0
+
+    sup_message = nil
+
+    source.poll do |sym, args|
+      assert_equal(sym, :add)
+      sup_message = Message.build_from_source source, args[:info]
+    end
+
+    from = sup_message.from
+    # "from" is just a simple person item
+
+    assert_equal("hbvv@yahoo.com", from.email)
+    #assert_equal("Fake Sender", from.name)
+
+    to = sup_message.to[0]
+    test_to = "a@b.c"
+
+    assert_equal(test_to, to.email)
+
+    subj = sup_message.subj
+    test_subj = "here comes a weird char: õ"
+    #assert_equal(test_subj, subj)
+
+    chunks = sup_message.load_from_source!
+    indexable_chunks = sup_message.indexable_chunks
+
+    # there should be only one chunk
+    #assert_equal(1, chunks.length)
+
+    lines = chunks[0].lines
+
+    # check if body content includes some of the expected text
+    #assert (lines.join.include? "check out: "), "Body message does not match expected value"
   end
 end
 
