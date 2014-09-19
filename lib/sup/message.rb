79d16576664e81ff07372b94831d70314ebbc32e
Scott Bonds <scott@ggr.com>
2014-09-19 10:01:06 -0700

    fix encoding before indexing

--- lib/sup/message.rb
+++ lib/sup/message.rb
@@ -335,13 +335,13 @@ EOS
       to.map { |p| p.indexable_content },
       cc.map { |p| p.indexable_content },
       bcc.map { |p| p.indexable_content },
-      indexable_chunks.map { |c| c.lines },
+      indexable_chunks.map { |c| c.lines.map { |l| l.fix_encoding! } },
       indexable_subject,
     ].flatten.compact.join " "
   end
 
   def indexable_body
-    indexable_chunks.map { |c| c.lines }.flatten.compact.join " "
+    indexable_chunks.map { |c| c.lines }.flatten.compact.map { |l| l.fix_encoding! }.join " "
   end
 
   def indexable_chunks
