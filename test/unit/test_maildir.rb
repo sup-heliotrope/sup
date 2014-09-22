208480f1127281539fd65c0e802c1b72226fe51c
Scott Bonds <scott@ggr.com>
2014-09-22 13:19:12 -0700

    stub maildir and mbox tests

---
+++ test/unit/test_maildir.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+describe Redwood::Maildir do
+  it "can index an maildir directory" do
+    assert_equal(false, true)
+  end
+  it "can index an maildir directory with special characters" do
+    assert_equal(false, true)
+  end
+  it "can display an email from an maildir directory" do
+    assert_equal(false, true)
+  end
+  it "can display an email from an maildir directory with special characters" do
+    assert_equal(false, true)
+  end
+end
+
---
+++ test/unit/test_mbox.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+describe Redwood::MBox do
+  it "can index an mbox file" do
+    assert_equal(false, true)
+  end
+  it "can index an mbox file with special characters" do
+    assert_equal(false, true)
+  end
+  it "can display an email from an mbox file" do
+    assert_equal(false, true)
+  end
+  it "can display an email from an mbox file with special characters" do
+    assert_equal(false, true)
+  end
+end
+
