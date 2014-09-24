c7e0b228a8f4c7cb0b9105d96b46f67e06d8480f
Scott Bonds <scott@ggr.com>
2014-09-24 16:07:02 -0700

    experiments with tests

--- test/unit/test_maildir.rb
+++ test/unit/test_maildir.rb
@@ -1,17 +1,62 @@
 require "test_helper"
 
-describe Redwood::Maildir do
-  it "can index an maildir directory" do
+class TestMaildir < Minitest::Test
+
+  def setup
+    @path = Dir.mktmpdir
+    @maildir = File.join @path, 'test_maildir'
+    ['', 'cur', 'new', 'tmp'].each do |dir|
+      Dir.mkdir(File.join @maildir, dir)
+    end
+    @label_service = LabelService.new
+    create_a_maildir_email(File.join(@maildir, 'cur'), <<EOS)
+From sup-talk-bounces@rubyforge.org Mon Apr 27 12:56:18 2009
+From: Bob <bob@bob.com>
+To: a dear friend
+
+Hello there friend. How are you?
+
+From bob@bob.com Mon Apr 27 12:56:19 2009
+From: Bob <bob@bob.com>
+To: a dear friend
+
+Hello again! Would you like to buy my products?
+EOS
+
+  end
+
+  def teardown
+    FileUtils.rm_r @path
+  end
+
+  def create_a_maildir_email(folder, content)
+    File.write(File.join(folder, "#{Time.now.to_f}.hostname:2,S"), content)
+  end
+
+  def test_can_index_a_maildir_directory
+    md = Maildir.new "maildir:#{@maildir}"
+    start
+    Index.init @path
+    Index.load
+    # SourceManager.instance.instance_eval '@sources = {}'
+    # SourceManager.instance.add_source md
+    # PollManager.poll_from md
+    # Index.save
+    require 'pry'; binding.pry
     assert_equal(false, true)
   end
-  it "can index an maildir directory with special characters" do
+
+  def test_can_index_a_maildir_directory_with_special_characters
     assert_equal(false, true)
   end
-  it "can display an email from an maildir directory" do
+
+  def test_can_display_an_email_from_a_maildir_directory
     assert_equal(false, true)
   end
-  it "can display an email from an maildir directory with special characters" do
+
+  def test_can_display_an_email_from_a_maildir_directory_with_special_characters
     assert_equal(false, true)
   end
+
 end
 
--- test/unit/test_mbox.rb
+++ test/unit/test_mbox.rb
@@ -1,17 +1,12 @@
 require "test_helper"
+require "test_header_parsing"
 
-describe Redwood::MBox do
-  it "can index an mbox file" do
-    assert_equal(false, true)
-  end
-  it "can index an mbox file with special characters" do
-    assert_equal(false, true)
-  end
-  it "can display an email from an mbox file" do
-    assert_equal(false, true)
-  end
-  it "can display an email from an mbox file with special characters" do
-    assert_equal(false, true)
+# verify that an mbox with special characters in the URI work normally
+# piggybacking on existing mbox tests
+class TestMBoxParsingSpecial < TestMBoxParsing
+  def setup
+    @path = Dir.mktmpdir
+    @mbox = File.join(@path, "test#{URI_ENCODE_CHARS}.mbox")
   end
 end
 
