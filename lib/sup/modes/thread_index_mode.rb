1e7fc5bc688bd7cde48e137cb81f094142630138
Gaute Hope <eg@gaute.vetsj.com>
2014-11-18 10:03:31 +0100

    buffer is nil sometimes, just ignore
    apparently the buffer is gone before the operation is finished
    sometimes, it seems that the standard practice throughout the codebase
    is to just check if the buffer is not nil (evaluates to true).

--- lib/sup/modes/thread_index_mode.rb
+++ lib/sup/modes/thread_index_mode.rb
@@ -1026,7 +1026,7 @@ private
   end
 
   def from_width
-    [(buffer.content_width.to_f * 0.2).to_i, MIN_FROM_WIDTH].max
+    [(buffer.content_width.to_f * 0.2).to_i, MIN_FROM_WIDTH].max if buffer else MIN_FROM_WIDTH # not sure why the buffer is gone
   end
 
   def initialize_threads
