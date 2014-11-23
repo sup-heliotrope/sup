c492bac842cd51193ce4e21f042b6998d220e3a3
Gaute Hope <eg@gaute.vetsj.com>
2014-11-23 12:32:00 +0100

    draft: ensure writing and reading is done in UTF-8

--- lib/sup/draft.rb
+++ lib/sup/draft.rb
@@ -16,7 +16,7 @@ class DraftManager
   def write_draft
     offset = @source.gen_offset
     fn = @source.fn_for_offset offset
-    File.open(fn, "w") { |f| yield f }
+    File.open(fn, "w:UTF-8") { |f| yield f }
     PollManager.poll_from @source
   end
 
@@ -85,7 +85,7 @@ class DraftLoader < Source
 
   def raw_header offset
     ret = ""
-    File.open fn_for_offset(offset) do |f|
+    File.open(fn_for_offset(offset), "r:UTF-8") do |f|
       until f.eof? || (l = f.gets) =~ /^$/
         ret += l
       end
@@ -94,13 +94,13 @@ class DraftLoader < Source
   end
 
   def each_raw_message_line offset
-    File.open(fn_for_offset(offset)) do |f|
+    File.open(fn_for_offset(offset), "r:UTF-8") do |f|
       yield f.gets until f.eof?
     end
   end
 
   def raw_message offset
-    IO.read(fn_for_offset(offset))
+    IO.read(fn_for_offset(offset), :encoding => "UTF-8")
   end
 
   def start_offset; 0; end
