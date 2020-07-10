cb9c7683e73381a4f4afc2a7b4f2de713b3d4555
Dan Callaghan <djc@djc.id.au>
2020-07-10 18:50:12 +1000

    add a suffix to the version to indicate when running from a git checkout

--- lib/sup/version.rb
+++ lib/sup/version.rb
@@ -1,3 +1,12 @@
+def git_suffix
+  revision = `GIT_DIR=#{__dir__}/../../.git git rev-parse HEAD 2>/dev/null`
+  if revision.empty?
+    "-git-unknown"
+  else
+    "-git-#{revision[0..7]}"
+  end
+end
+
 module Redwood
-  VERSION = "0.22.1"
+  VERSION = "0.22.1#{git_suffix}"
 end
