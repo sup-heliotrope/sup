88779f92368cb85057d10dce751c04ef15cee803
Dan Callaghan <djc@djc.id.au>
2023-03-29 21:41:13 +1100

    also test against Ruby 3.2 in CI

---
+++ contrib/nix/ruby3.2-shell.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
+    ref = "refs/heads/master";
+    rev = "402cc3633cc60dfc50378197305c984518b30773";
+  }) {};
+  gems = pkgs.bundlerEnv {
+    name = "ruby3.2-gems-for-sup";
+    ruby = pkgs.ruby_3_2;
+    gemfile = ./Gemfile;
+    lockfile = ./Gemfile.lock;
+    gemset = ./gemset.nix;
+  };
+in pkgs.mkShell { packages = [ gems gems.wrappedRuby pkgs.pandoc ]; }
