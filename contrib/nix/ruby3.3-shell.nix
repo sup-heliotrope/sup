07e633cb6af7f2cb947143b64a29e774b61d3c20
Dan Callaghan <djc@djc.id.au>
2025-03-29 18:19:05 +1100

    contrib/nix: bump nixpkgs
    This is to get ruby_3_4.

--- contrib/nix/ruby3.3-shell.nix
+++ contrib/nix/ruby3.3-shell.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs";
     ref = "refs/heads/master";
-    rev = "0e2ba0d131331e318eba20fcb03db0372dc2a926";
+    rev = "5d0ebea1934d80948ff7b84f3b06e4ec9d99ee49";
   }) {};
   gems = pkgs.bundlerEnv {
     name = "ruby3.3-gems-for-sup";
