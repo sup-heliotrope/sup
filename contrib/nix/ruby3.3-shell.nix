1a8e63f3fe2549ad27684192a6fd80bc0dbdce24
Dan Callaghan <djc@djc.id.au>
2025-04-05 15:04:33 +1100

    contrib/nix: build ncurses with --disable-opaque-curses
    Works around #623.

--- contrib/nix/ruby3.3-shell.nix
+++ contrib/nix/ruby3.3-shell.nix
@@ -4,6 +4,9 @@ let
     ref = "refs/heads/master";
     rev = "5d0ebea1934d80948ff7b84f3b06e4ec9d99ee49";
   }) {};
+  ncurses = pkgs.ncurses.overrideAttrs (finalAttrs: previousAttrs: {
+    configureFlags = previousAttrs.configureFlags ++ [ "--disable-opaque-curses" ];
+  });
   gems = pkgs.bundlerEnv {
     name = "ruby3.3-gems-for-sup";
     ruby = pkgs.ruby_3_3;
@@ -18,6 +21,14 @@ let
           url = "https://github.com/danc86/xapian-ruby/releases/download/v1.4.27/xapian-ruby-1.4.27.gem";
           sha256 = "sha256-E5U/4NEFkChMJtrMj8oCqKPgTYyKku5OXGcPQvhN4xM=";
         };
+      };
+      # Workaround for Sup issue #623
+      ncursesw = attrs: pkgs.defaultGemConfig.ncursesw attrs // {
+        buildInputs = [ ncurses ];
+        buildFlags = [
+          "--with-cflags=-I${ncurses.dev}/include"
+          "--with-ldflags=-L${ncurses.out}/lib"
+        ];
       };
     };
   };
