7a444112c8d915515d6a90644cf2b4e86000fa30
Dan Callaghan <djc@djc.id.au>
2025-04-06 10:20:20 +1000

    contrib/nix: use patched ncursesw-ruby instead of modifying ncurses

--- contrib/nix/ruby3.3-shell.nix
+++ contrib/nix/ruby3.3-shell.nix
@@ -4,9 +4,6 @@ let
     ref = "refs/heads/master";
     rev = "5d0ebea1934d80948ff7b84f3b06e4ec9d99ee49";
   }) {};
-  ncurses = pkgs.ncurses.overrideAttrs (finalAttrs: previousAttrs: {
-    configureFlags = previousAttrs.configureFlags ++ [ "--disable-opaque-curses" ];
-  });
   gems = pkgs.bundlerEnv {
     name = "ruby3.3-gems-for-sup";
     ruby = pkgs.ruby_3_3;
@@ -16,11 +13,12 @@ let
     gemConfig = pkgs.defaultGemConfig // {
       # Workaround for Sup issue #623
       ncursesw = attrs: pkgs.defaultGemConfig.ncursesw attrs // {
-        buildInputs = [ ncurses ];
-        buildFlags = [
-          "--with-cflags=-I${ncurses.dev}/include"
-          "--with-ldflags=-L${ncurses.out}/lib"
-        ];
+        src = pkgs.fetchFromGitHub {
+          owner = "danc86";
+          repo = "ncursesw-ruby";
+          rev = "43cfa21f781e9412dc73d0d4a44b3ec0bf4a3c8d";
+          hash = "sha256-MkXFwhbtL9aJOMqn1IR5DKMXcnKHzICjb/rVhDDLL94=";
+        };
       };
     };
   };
