07ae55696a4121295386b4cd6ae498b2c00254a0
Dan Callaghan <djc@djc.id.au>
2025-04-10 19:44:22 +1000

    contrib/nix: patch rmail for frozen string literals

--- contrib/nix/ruby3.4-shell.nix
+++ contrib/nix/ruby3.4-shell.nix
@@ -11,6 +11,17 @@ let
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
     gemConfig = pkgs.defaultGemConfig // {
+      rmail = attrs: {
+        dontBuild = false;
+        patches = [
+          # Frozen string literals: https://github.com/terceiro/rmail/pull/13
+          (pkgs.fetchpatch2 {
+            name = "rmail-frozen-string-literals.patch";
+            url = "https://github.com/terceiro/rmail/pull/13/commits/27f455af1fea0be0aa09959cc2237cbdf68de2a1.patch";
+            hash = "sha256-N5X9zix+WPoEugp2DBTu7dRDmesrF5pT/8Td2wraYoA=";
+          })
+        ];
+      };
       # Workaround for Sup issue #623
       ncursesw = attrs: pkgs.defaultGemConfig.ncursesw attrs // {
         src = pkgs.fetchFromGitHub {
